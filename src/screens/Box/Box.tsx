3bba686df44251742a3206b1bd32b89ae5537eb4
dsmamaeva <dsmamaeva0412@gmail.com>
2025-11-19 22:25:25 +0300

    Create Box.tsx

---
+++ src/screens/Box/Box.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Box = (): JSX.Element => {
+  return (
+    <div className="w-[2711px] h-[8996px]" data-model-id="51:778-frame">
+      <img
+        className="fixed top-0 left-0 w-[2711px] h-[8996px]"
+        alt="Home"
+        src="https://c.animaapp.com/dauW3qoa/img/home.svg"
+      />
+    </div>
+  );
+};
